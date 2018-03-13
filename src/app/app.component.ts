1334b8c5ce5a2f20639e324c8596cd03dcad94a9
Bielik <bielik20@gmail.com>
2018-03-13 19:36:07 +0100

    Update Angular and .angular-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Foo } from '@my/foo';
+import { Bar } from '@my/bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  foo = new Foo();
+  bar = new Bar();
+  server$: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.server$ = this.http.get('api/public/simple');
+  }
 }
